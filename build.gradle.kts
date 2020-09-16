74d36e1fa5a9e407fd8035433df10f699395ed81
Jesus Zazueta <jjzazuet@vacco.io>
2020-09-16 18:23:29 -0400

    - Build fixes.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,20 +2,11 @@ buildscript {
   repositories { maven { name = "VaccoOss"; setUrl("https://dl.bintray.com/vaccovecrana/vacco-oss") } }
   dependencies { classpath("io.vacco.common:common-build:0.1.0") }
 }
-plugins { `java-gradle-plugin` }
+
 apply(from = project.buildscript.classLoader.getResource("io/vacco/common/java-library.gradle.kts").toURI())
 
 group = "io.vacco"
 version = "1.6.0"
-
-gradlePlugin {
-  plugins {
-    create("classpath-hell-gradle-plugin") {
-      id = "io.vacco.classpath-hell-gradle-plugin"
-      implementationClass = "io.vacco.gradle.plugin.cphell.ClasspathHellPlugin"
-    }
-  }
-}
 
 configure<JavaPluginExtension> {
   sourceCompatibility = JavaVersion.VERSION_1_8
