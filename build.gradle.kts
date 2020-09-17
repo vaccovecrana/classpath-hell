89efda51c0a07f428dd893c28cc5a5076c842759
Jesus Zazueta <jjzazuet@vacco.io>
2020-09-17 09:01:35 -0400

    - Removing common build.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,58 @@
-buildscript {
-  repositories { maven { name = "VaccoOss"; setUrl("https://dl.bintray.com/vaccovecrana/vacco-oss") } }
-  dependencies { classpath("io.vacco.common:common-build:0.1.0") }
-}
+repositories { jcenter() }
 
-apply(from = project.buildscript.classLoader.getResource("io/vacco/common/java-library.gradle.kts").toURI())
+plugins { `java-library`; jacoco; `maven-publish` }
 
 group = "io.vacco"
 version = "1.6.0"
 
+dependencies {
+  api(gradleApi())
+  testImplementation("io.github.j8spec:j8spec:3.0.0")
+  testRuntimeOnly("org.hamcrest:hamcrest-all:1.3")
+  testRuntimeOnly("org.hamcrest:hamcrest-core:1.3")
+}
+
 configure<JavaPluginExtension> {
   sourceCompatibility = JavaVersion.VERSION_1_8
   targetCompatibility = JavaVersion.VERSION_1_8
+  withJavadocJar()
+  withSourcesJar()
 }
 
-val api by configurations
-val testRuntimeOnly by configurations
-dependencies {
-  api(gradleApi())
-  testRuntimeOnly("org.hamcrest:hamcrest-all:1.3")
-  testRuntimeOnly("org.hamcrest:hamcrest-core:1.3")
+tasks.withType<JavaCompile> { options.compilerArgs.add("-Xlint:all") }
+tasks.withType<Test> { this.testLogging { this.showStandardStreams = true } }
+
+val tokenFile = File(System.getProperty("user.home"), ".vaccoToken").readText()
+val libraryDesc: String by project
+val licenseName: String by project
+val licenseUrl: String by project
+val gitUrl: String by project
+val siteUrl: String by project
+
+configure<PublishingExtension> {
+  publications {
+    create<MavenPublication>("vaccoOss") {
+      from(components["java"])
+      pom {
+        name.set(project.name)
+        description.set(libraryDesc)
+        url.set(siteUrl)
+        licenses { license { name.set(licenseName); url.set(licenseUrl) } }
+        developers {
+          developer { id.set("vacco"); name.set("Vaccove Crana, LLC."); email.set("humans@vacco.io") }
+        }
+        scm { connection.set(gitUrl); developerConnection.set(gitUrl); url.set(siteUrl) }
+      }
+    }
+  }
+  repositories {
+    maven {
+      name = "VaccoOss"
+      setUrl("https://api.bintray.com/maven/vaccovecrana/vacco-oss/${project.name}")
+      credentials {
+        username = tokenFile.split(":")[0].trim()
+        password = tokenFile.split(":")[1].trim()
+      }
+    }
+  }
 }
