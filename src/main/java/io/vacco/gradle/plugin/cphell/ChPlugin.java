571171e310c2c65b4052f088eca30dfbf0e44387
Jesus Zazueta <jjzazuet@vacco.io>
2020-09-17 13:21:54 -0400

    - Extension fixes.

--- src/main/java/io/vacco/gradle/plugin/cphell/ChPlugin.java
+++ src/main/java/io/vacco/gradle/plugin/cphell/ChPlugin.java
@@ -5,7 +5,7 @@ import org.gradle.api.Project;
 
 public class ChPlugin implements Plugin<Project> {
   public void apply(Project project) {
-    project.getExtensions().create("classpathHell", ChDefaultPluginExtension.class);
+    project.getExtensions().create("classpathHell", ChPluginExtension.class);
     project.getTasks().create("checkClasspath", ChTask.class);
   }
 }
